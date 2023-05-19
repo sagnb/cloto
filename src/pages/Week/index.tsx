603983a01f410ba6be3eaf9b6a279204c205213a
Guilherme Neri Bustamante Sá <guinbsa@gmail.com>
2023-05-19 00:48:46 -0300

    update week view

--- src/pages/Week/index.tsx
+++ src/pages/Week/index.tsx
@@ -3,10 +3,11 @@ import moment from 'moment'
 
 import WeekDaysNames from '../../components/WeekDaysNames'
 import WeekRow from '../../components/WeekRow'
+import DayColumn from '../../components/DayColumn'
 
 import './styles.scss'
 
-export default function Day(){
+export default function Week(){
   const { weekDisplay } = useParams()
   const [ week, month, year ] = weekDisplay?weekDisplay.split(' '):['-1', '',  '-1']
   const currentWeek = moment().week(Number(week)+1).year(Number(year))
@@ -15,13 +16,24 @@ export default function Day(){
   const day = startDay.clone().subtract(1, 'day')
   const weekDaysNames = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
   const weekDays = Array(7).fill(0).map(() => day.add(1, 'day').clone())
+  const hours = Array(24).fill(0).map((_, index) => index)
   const currentDay = moment()
 
   return (
     <div className='Week'>
-      <WeekDaysNames daysNames={weekDaysNames} currentYear={Number(year)} monthName={currentWeek.format('MMMM')}/>
+      <div className="WeekLeftContainer">
+        { hours.map(hour => <div className='Hour' key={'Week' + week + 'Hour' + hour.toString()}>{hour}</div>) }
+      </div>
 
-      <WeekRow currentYear={Number(year)} month={currentMonth} days={weekDays} index={Number(week+year)} currentDay={currentDay}/>
+      <div className='WeekRightContainer'>
+        <WeekDaysNames daysNames={weekDaysNames} currentYear={Number(year)} monthName={currentWeek.format('MMMM')}/>
+
+        <WeekRow currentYear={Number(year)} month={currentMonth} days={weekDays} index={Number(week+year)} currentDay={currentDay}/>
+
+        <div className='DayColumnsContainer'>
+          { weekDays.map(day => <DayColumn key={day.format('DD MMMM YYYY') + 'DayColumn'}/>) }
+        </div>
+      </div>
     </div>
   )
 }
